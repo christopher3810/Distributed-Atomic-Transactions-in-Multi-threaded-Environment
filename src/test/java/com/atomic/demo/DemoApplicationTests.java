8273a798361560c7609dce4a75a5392cd4247cbb
smyang <smyang@suresofttech.com>
2023-10-31 06:58:47 +0900

    [Feat] : Spring boot project init

---
+++ src/test/java/com/atomic/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

981cb40e4b2886547acf1a6d2f5bbcc8b5f3309d
sungminyang <lonesome661@naver.com>
2023-11-04 20:21:02 +0900

    [Feat] : Lock 해제하는 함수 생성, Controller 에서 Lock 생성 및 lock 해제하는 로직 추가
    Signed-off-by: sungminyang <lonesome661@naver.com>

--- src/main/java/com/atomic/demo/domain/payment/rest/PaymentController.java
+++ src/main/java/com/atomic/demo/domain/payment/rest/PaymentController.java
@@ -3,6 +3,7 @@ package com.atomic.demo.domain.payment.rest;
 import com.atomic.demo.domain.payment.model.dto.PaymentRequestDto;
 import com.atomic.demo.domain.payment.model.dto.PaymentResponseDto;
 import com.atomic.demo.domain.payment.service.PaymentService;
+import com.atomic.demo.domain.redisLock.service.RedisLockService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
 @RestController
 @RequestMapping("/api/payment")
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ import reactor.core.publisher.Mono;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final RedisLockService lockService;
 
     @Operation(summary = "결제 요청", description = "카드 정보로 결제를 요청 합니다.")
     @PostMapping("/pay")
@@ -32,9 +36,13 @@ public class PaymentController {
             required = true
         )
         @Valid @RequestBody PaymentRequestDto paymentRequest) {
-        return paymentService.savePaymentData(paymentRequest)
-            .map(ResponseEntity::ok)
-            .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        String cardNumber = paymentRequest.getCardInfo().getCardNumber();
+        return lockService.acquireLock(cardNumber)
+            .flatMap(locked -> paymentService.savePaymentData(paymentRequest)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).build()))
+            .publishOn(Schedulers.boundedElastic())
+            .doFinally(signalType -> lockService.releaseLock(cardNumber).subscribe());
     }
 
 }
--- src/main/java/com/atomic/demo/domain/redisLock/service/RedisLockService.java
+++ src/main/java/com/atomic/demo/domain/redisLock/service/RedisLockService.java
@@ -1,7 +1,7 @@
-package com.atomic.demo.domain.lock.service;
+package com.atomic.demo.domain.redisLock.service;
 
-import com.atomic.demo.domain.lock.exception.LockAcquisitionException;
-import com.atomic.demo.domain.lock.model.LockSettings;
+import com.atomic.demo.domain.redisLock.exception.LockAnquisitionException;
+import com.atomic.demo.domain.redisLock.model.LockSettings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -24,11 +24,14 @@ public class RedisLockService {
         return reactiveRedisTemplate.opsForValue()
             .setIfAbsent(lockKey, lockValue, LockSettings.DEFAULT.lockTtl())
             .filter(Boolean.TRUE::equals)
-            .switchIfEmpty(Mono.error(new LockAcquisitionException("Unable to acquire lock Because Lock already Exist : " + lockKey)))
+            .switchIfEmpty(Mono.error(new LockAnquisitionException("Unable to acquire lock Because Lock already Exist : " + lockKey)))
             .thenReturn(true);
     }
 
-    public Mono<Boolean> releaseLock(String lockKey) {
-        return reactiveRedisTemplate.opsForValue().delete(lockKey);
+    public Mono<Boolean> releaseLock(String cardNumber) {
+        String lockKey = LockSettings.DEFAULT.lockPrefix() + cardNumber;
+        return reactiveRedisTemplate.opsForValue().delete(lockKey)
+            .doOnSuccess(result -> {/* 락 해제 성공 로그 등 처리 */})
+            .doOnError(error -> {/* 락 해제 실패 로그 등 처리 */});
     }
 }

5ab203c457a71bfd42d856f79cc395e068a6dfce
sungminyang <lonesome661@naver.com>
2023-11-04 15:01:37 +0900

    [Feat] : vat 공백 제거후 반환하는 deserializer 추가
    Signed-off-by: sungminyang <lonesome661@naver.com>

---
+++ src/main/java/com/atomic/demo/application/helper/VatDeserializer.java
@@ -0,0 +1,18 @@
+package com.atomic.demo.application.helper;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import java.io.IOException;
+import java.util.Optional;
+
+public class VatDeserializer extends JsonDeserializer<Optional<String>> {
+    @Override
+    public Optional<String> deserialize(JsonParser p, DeserializationContext txt) throws IOException {
+        String value = p.getValueAsString();
+        if (value == null || value.trim().isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(value.trim());
+    }
+}

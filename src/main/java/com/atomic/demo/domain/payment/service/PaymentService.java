a64f3e48f572c9518d35c93611f71ef7bf7bb83b
sungminyang <lonesome661@naver.com>
2023-11-04 15:19:24 +0900

    [Feat] : PaymentService 껍데기 서비스 생성
    Signed-off-by: sungminyang <lonesome661@naver.com>

---
+++ src/main/java/com/atomic/demo/domain/payment/service/PaymentService.java
@@ -0,0 +1,13 @@
+package com.atomic.demo.domain.payment.service;
+
+import com.atomic.demo.application.dto.payment.PaymentResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public PaymentResponseDto savePaymentData(PaymentResponseDto responseDto){
+        return null;
+    }
+
+}

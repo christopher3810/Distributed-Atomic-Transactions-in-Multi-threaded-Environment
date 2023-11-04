440faa5b3b95fc63c9658aa94a3d61b86d8cce1d
sungminyang <lonesome661@naver.com>
2023-11-04 15:41:16 +0900

    [Feat] : Controller, Request 설명 추가, savePayemnt 컨트롤러 반환 흐름 생성
    Signed-off-by: sungminyang <lonesome661@naver.com>

--- src/main/java/com/atomic/demo/interfaces/rest/PaymentController.java
+++ src/main/java/com/atomic/demo/interfaces/rest/PaymentController.java
@@ -2,8 +2,14 @@ package com.atomic.demo.interfaces.rest;
 
 import com.atomic.demo.application.dto.payment.PaymentRequestDto;
 import com.atomic.demo.application.dto.payment.PaymentResponseDto;
+import com.atomic.demo.domain.payment.service.PaymentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +19,23 @@ import reactor.core.publisher.Mono;
 @Tag(name = "Payment API", description = "Payment services")
 @RestController
 @RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
 
-    @Operation(summary = "Create a new payment", description = "Creates a new payment transaction")
-    @PostMapping
-    public Mono<PaymentResponseDto> createPayment(@RequestBody PaymentRequestDto paymentRequest) {
+    private final PaymentService paymentService;
 
+    @Operation(summary = "결제 요청", description = "카드 정보로 결제를 요청 합니다.")
+    @PostMapping
+    public Mono<ResponseEntity<PaymentResponseDto>> createPayment(
+        @Parameter(
+            name = "payment Request",
+            description = "결제 정보",
+            required = true
+        )
+        @Valid @RequestBody PaymentRequestDto paymentRequest) {
+        return paymentService.savePaymentData(paymentRequest)
+            .map(paymentResponseDto -> ResponseEntity.ok(paymentResponseDto))
+            .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 }
